b29c44edd346fc4e65a023122630fb56fe25c9bd
S1urppy <dylondominikus@gmail.com>
2026-02-02 23:58:54 +0700

    week01: (tugas 2) implement safety for user note

--- src/oop_115708_DominikusDylon/week01/GameStore.kt
+++ src/oop_115708_DominikusDylon/week01/GameStore.kt
@@ -5,11 +5,13 @@ import com.sun.jdi.IntegerType
 fun main (){
     val gameTitle: String
     val price: Int
+    var userNote: String? = null
 
-    printRecipt(title = "Tomb Rider", finalPrice = calculateDiscount(price = 200000))
+    printRecipt(title = "Tomb Rider", finalPrice = calculateDiscount(price = 200000), userNote)
 }
 
 fun calculateDiscount(price: Int): Int = if(price > 500000) price*80/100 else price*90/100
-fun printRecipt(title: String, finalPrice: Int){
+fun printRecipt(title: String, finalPrice: Int, userNote: String?){
         println("title: $title, finalPrice: $finalPrice")
+    println(if (userNote?.length == null) "Tidak ada catatan" else userNote)
 }
