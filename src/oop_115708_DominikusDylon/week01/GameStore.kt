e152202501833851bc3100bad2370f47d24726d6
S1urppy <dylondominikus@gmail.com>
2026-02-02 23:01:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_115708_DominikusDylon/week01/GameStore.kt
+++ src/oop_115708_DominikusDylon/week01/GameStore.kt
@@ -3,6 +3,8 @@ package oop_115708_DominikusDylon.week01
 import com.sun.jdi.IntegerType
 
 fun main (){
-    val gameTitle: String
-    val price : Int
+    val gameTitle: String = "Tomb Rider"
+    val price: Int = 200000
 }
+
+fun calculateDiscount(price: Int): Int = if(price > 500000) price*80/100 else price*90/100
