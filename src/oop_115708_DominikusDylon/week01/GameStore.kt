f45b04a549f1968282cc353d675972ec82ac4959
S1urppy <dylondominikus@gmail.com>
2026-02-02 23:57:34 +0700

    week01: (tugas 2) implement recipt printing

--- src/oop_115708_DominikusDylon/week01/GameStore.kt
+++ src/oop_115708_DominikusDylon/week01/GameStore.kt
@@ -3,8 +3,13 @@ package oop_115708_DominikusDylon.week01
 import com.sun.jdi.IntegerType
 
 fun main (){
-    val gameTitle: String = "Tomb Rider"
-    val price: Int = 200000
+    val gameTitle: String
+    val price: Int
+
+    printRecipt(title = "Tomb Rider", finalPrice = calculateDiscount(price = 200000))
 }
 
 fun calculateDiscount(price: Int): Int = if(price > 500000) price*80/100 else price*90/100
+fun printRecipt(title: String, finalPrice: Int){
+        println("title: $title, finalPrice: $finalPrice")
+}
