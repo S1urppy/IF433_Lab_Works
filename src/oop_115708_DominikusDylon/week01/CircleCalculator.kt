7d87b4992c29a76dd37be0d735241c619e87c230
S1urppy <dylondominikus@gmail.com>
2026-02-02 22:35:52 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_115708_DominikusDylon/week01/CircleCalculator.kt
+++ src/oop_115708_DominikusDylon/week01/CircleCalculator.kt
@@ -3,15 +3,15 @@ package oop_115708_DominikusDylon.week01
 import java.awt.geom.Area
 
 fun main(args: Array<String>){
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    var radius = 7.0
+    val pi = 3.14
 
-    var area: Double = pi * radius * radius
-    println("Radius: " + radius + ", Area: " + area)
+    var area = pi * radius * radius
+    println("Radius: ${radius}, Area: ${area}")
     checkSize(area)
 }
 
-fun checkSize(area: Double) {
+fun checkSize(area : Double) {
     if (area > 100) {
         println("This is a Big Circle")
     } else {
--- src/oop_115708_DominikusDylon/week01/GradingSystem.kt
+++
@@ -1,8 +0,0 @@
-package oop_115708_DominikusDylon.week01
-
-fun main (){
-    var name: String = "John Thor"
-    var score: Int = 80
-
-    println("Nama: " + name + ", Nilai: " + score)
-}
