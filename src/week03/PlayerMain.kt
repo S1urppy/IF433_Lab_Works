788672d490503a229d2c301da33d78fdaead5115
S1urppy <dylondominikus@gmail.com>
2026-02-22 13:15:25 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/week03/PlayerMain.kt
@@ -0,0 +1,11 @@
+package week03
+
+fun main() {
+
+    val player = Player("Xebec")
+
+    // println(player.xp) ERROR
+    player.addXp(50)   // XP = 50 → Level 1
+    player.addXp(60)   // XP = 110 → Level Up ke 2
+
+}
