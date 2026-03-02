d5b4916ed62d383c08e19992ca13fe41cce818fd
S1urppy <dylondominikus@gmail.com>
2026-03-02 23:02:04 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_115708_DominikusDylon/Week05/TaskMain.kt
@@ -0,0 +1,9 @@
+package oop_115708_DominikusDylon.Week05
+
+fun main() {
+    val math = MathHelper()
+
+    println(math.hitungLuas(4))
+    println(math.hitungLuas(6,4))
+    println(math.hitungLuas(4.4))
+}
