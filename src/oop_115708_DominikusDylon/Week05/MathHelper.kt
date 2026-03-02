5191a255f296e029475ea4aeb02f9d4e353c13de
S1urppy <dylondominikus@gmail.com>
2026-03-02 22:56:23 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_115708_DominikusDylon/Week05/MathHelper.kt
@@ -0,0 +1,14 @@
+package oop_115708_DominikusDylon.Week05
+
+class MathHelper {
+    fun hitungLuas (sisi: Int): Int {
+        return (sisi * sisi)
+    }
+    fun hitungLuas (panjang: Int, lebar: Int): Int {
+        return (panjang * lebar)
+    }
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+
+}
