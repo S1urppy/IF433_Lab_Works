2f480c948baca9f470e0c6b2930a8fedbaadc764
S1urppy <dylondominikus@gmail.com>
2026-03-02 23:38:53 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_115708_DominikusDylon/Week05/TaskMain.kt
+++ src/oop_115708_DominikusDylon/Week05/TaskMain.kt
@@ -6,4 +6,13 @@ fun main() {
     println(math.hitungLuas(4))
     println(math.hitungLuas(6,4))
     println(math.hitungLuas(4.4))
+
+
+    val EWallet = EWallet("Yanto", 50000.00)
+    val CreditCard = CreditCard("Ahmad", 100000.00)
+
+    val payments: List<PaymentMethod> =listOf(EWallet, CreditCard)
+    for (method in payments){
+        method.processPayment(75000.00)
+    }
 }
