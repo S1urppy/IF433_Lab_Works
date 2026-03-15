c605c12ebdbf67966151150e68d3e7510b5eff97
S1urppy <dylondominikus@gmail.com>
2026-03-15 21:09:48 +0700

    week06: (task) create SmartCCTV with compount interfaces

---
+++ src/oop_115708_DominikusDylon/Week06/SmartCCTV.kt
@@ -0,0 +1,18 @@
+package oop_115708_DominikusDylon.Week06
+
+class SmartCCTV(override val id: String, override val name: String) : smartDevice, Switchable, Recordable {
+    override fun turnOn(){
+        println("$name dinyalakan")
+        startRecord()
+    }
+
+    override fun turnOff(){
+        println("$name dimatikan")
+        stopRecord()
+    }
+
+    override fun startRecord(){
+        println("$name memulai record")
+    }
+
+}
