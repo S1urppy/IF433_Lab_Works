c82d9b3270e5e2462c11e29831f237b1d4955c07
S1urppy <dylondominikus@gmail.com>
2026-03-15 21:27:25 +0700

    (week06: (task) implement advanced smart casting in Hub

--- src/oop_115708_DominikusDylon/Week06/SmartHomeHub.kt
+++ src/oop_115708_DominikusDylon/Week06/SmartHomeHub.kt
@@ -1,5 +1,7 @@
 package oop_115708_DominikusDylon.Week06
 
+import java.security.Security
+
 class SmartHomeHub(val devices: MutableList<smartDevice> = mutableListOf()) {
     fun addDevice(device: smartDevice){
         devices.add(device)
@@ -9,6 +11,17 @@ class SmartHomeHub(val devices: MutableList<smartDevice> = mutableListOf()) {
         for(device in devices){
             if (device is Switchable){
                 device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode(){
+        for (device in devices){
+            if (device is Recordable){
+                device.startRecord()
+            }
+            if (device is SmartSpeaker){
+                device.playMusic("Sirine Peringatan")
             }
         }
     }
