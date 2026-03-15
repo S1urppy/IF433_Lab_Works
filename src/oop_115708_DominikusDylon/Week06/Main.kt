0386432c371aeacce1b692552d320afd1a96f1dd
S1urppy <dylondominikus@gmail.com>
2026-03-15 21:34:32 +0700

    week06: (task) instantiate smart device in main

--- src/oop_115708_DominikusDylon/Week06/Main.kt
+++ src/oop_115708_DominikusDylon/Week06/Main.kt
@@ -18,4 +18,10 @@ fun main(){
     println("\n=== TESTING CHECKKOUT ===")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
+
+
+    println("\n======SMARTHOME ENVIRONMENT======")
+    val lamp = SmartLamp("L01", "RuangTamu")
+    val smartSpeaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val SmartCCTV = SmartCCTV("C01", "Ezviz Garasi")
 }
