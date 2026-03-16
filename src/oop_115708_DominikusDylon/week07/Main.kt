e497a17c5e4ea41119f2df84ba058bceae0b7398
S1urppy <dylondominikus@gmail.com>
2026-03-16 13:35:18 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_115708_DominikusDylon/week07/Main.kt
+++ src/oop_115708_DominikusDylon/week07/Main.kt
@@ -1,15 +1,11 @@
+package oop_115708_DominikusDylon.week07
 
-class NetworkClient private constructor(val url: String) {
-    companion object {
-        const val BASE_URL = "https://api.umn.ac.ic"
+fun main() {
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
 
-        fun createClient(): NetworkClient {
-            println("membangun NetworkClient dengan BASE_URL: $BASE_URL")
-            return NetworkClient(BASE_URL)
-        }
-    }
-
-    fun connect() {
-        println("Connecting to $url...")
-    }
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
--- src/oop_115708_DominikusDylon/week07/NetworkClient.kt
+++ src/oop_115708_DominikusDylon/week07/NetworkClient.kt
@@ -1,7 +1,17 @@
 package oop_115708_DominikusDylon.week07
 
+
 class NetworkClient private constructor(val url: String) {
-    fun connect(){
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.ic"
+
+        fun createClient(): NetworkClient {
+            println("membangun NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+
+    fun connect() {
         println("Connecting to $url...")
     }
 }
