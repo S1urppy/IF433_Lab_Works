539a489a4d3603c1a7341aea03a2569d3ad9c349
S1urppy <dylondominikus@gmail.com>
2026-04-17 20:01:26 +0700

    week08: implement requireNotNull as safer alternative

--- src/oop_115708_DominikusDylon/Week08/Main.kt
+++ src/oop_115708_DominikusDylon/Week08/Main.kt
@@ -32,4 +32,21 @@ fun main(){
     val someObject: Any = 100
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast + fallback: $safeString")
+
+    println("\n=== TEST THE RED BUTTON ===")
+    val toxicData: String? = null
+    try {
+        val length = toxicData!!.length
+    } catch (e: NullPointerException){
+        println("CRASH (NPE)! Jangan gunakan !! secara sembarangan")
+    }
+
+    val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
+    try {
+        val token = requireNotNull(apiResponse["token"]) {
+            "CRITICAL EXCEPTION: Token otentikasi tidak ditemukan dari server!"
+        }
+    } catch (e: IllegalArgumentException){
+        println(e.message)
+    }
 }
