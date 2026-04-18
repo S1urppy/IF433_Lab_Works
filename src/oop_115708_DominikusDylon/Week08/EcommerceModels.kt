2492033612874282ceab992d7e8bc7562f43a1f6
S1urppy <dylondominikus@gmail.com>
2026-04-18 21:24:19 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_115708_DominikusDylon/Week08/EcommerceModels.kt
@@ -0,0 +1,6 @@
+package oop_115708_DominikusDylon.Week08
+
+sealed class Product
+
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+data class Clothing(val id: String, val name: String, val size: String) : Product()
