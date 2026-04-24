9863b36a11a78108f00a73040082bbe1d08ad115
S1urppy <dylondominikus@gmail.com>
2026-04-24 18:39:43 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_115708_DominikusDylon/Week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_115708_DominikusDylon.Week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, 8.7, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 25, 12.3, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 5, -2.0, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 30, 20.1, "CLOSED")
+    )
+
+}
