0fc81b578dc0d2df41ef4fb2d43aef2e0e841205
S1urppy <dylondominikus@gmail.com>
2026-04-30 23:38:49 +0700

    week10: (task) populate coin repository

--- src/oop_115708_DominikusDylon/Week10/CryptoDashboard.kt
+++ src/oop_115708_DominikusDylon/Week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_115708_DominikusDylon.Week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
