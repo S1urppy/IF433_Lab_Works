d4ad05dfdb0f0704b37460218b425784e11fc119
S1urppy <dylondominikus@gmail.com>
2026-04-30 23:18:45 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_115708_DominikusDylon/Week10/WalletRepository.kt
@@ -0,0 +1,12 @@
+package oop_115708_DominikusDylon.Week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
