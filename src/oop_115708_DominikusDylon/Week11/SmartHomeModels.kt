f569f94f93fbc7df0ab1c660c9ddada4e91f0540
S1urppy <dylondominikus@gmail.com>
2026-05-09 22:12:47 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_115708_DominikusDylon/Week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_115708_DominikusDylon.Week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false
+    var powerLoad: Int = 0
+)
