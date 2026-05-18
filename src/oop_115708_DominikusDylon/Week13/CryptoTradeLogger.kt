9693771f33e1de2abccb5ad1ecc2d41367738bd7
S1urppy <dylondominikus@gmail.com>
2026-05-18 16:25:03 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_115708_DominikusDylon/Week13/CryptoTradeLogger.kt
+++ src/oop_115708_DominikusDylon/Week13/CryptoTradeLogger.kt
@@ -65,4 +65,8 @@ fun main() {
     saveTrades(trades, "crypto_trades.csv")
 
     File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
+
+    val loadedData = loadTrades("crypto_trades.csv")
+    val totalPnL = loadedData.sumOf{ it.pnl }
+    println("Total PNL Bersih: $totalPnL")
 }
